cd861fdb2ef84f9397985720d2179c754c274935
Pat P <sell445@gmail.com>
2019-10-17 12:56:18 -0400

    Added ListGroup and Transition Group as well as functionality for displaying list

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavbar from './components/AppNavbar'
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
